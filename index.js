f7b783ee6df6bd366def2580d62c40aabe1d2208
CalebL42 <caleb_lee@ucsb.edu>
2025-09-02 18:58:03 -0700

    refactored to using express

--- index.js
+++ index.js
@@ -1,53 +1,26 @@
-import { createServer } from 'node:http';
-import fs from 'node:fs';
+import express from 'express';
+const app = express();
 
 const hostname = 'localhost';
 const port = 8080;
+const __dirname = import.meta.dirname;
+const options = {
+  root: __dirname,
+}
 
-const server = createServer((req, res) => {
-  const url = req.url;
-
-  res.setHeader('Content-Type', 'text/html');
-
-  if (url === '/') {
-    res.writeHead(200);
-    fs.readFile('./index.html', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      } 
-      res.end(data);
-    });
-  } else if (url === '/about') {
-    res.writeHead(200);
-    fs.readFile('./about.html', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      } 
-      res.end(data);
-    });
-  } else if (url === '/contact') {
-    res.writeHead(200);
-    fs.readFile('./contact-me.html', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      } 
-      res.end(data);
-    });
-  } else {
-    res.writeHead(404);
-    fs.readFile('./404.html', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      } 
-      res.end(data);
-    });
-  }
+app.get('/', (req, res) => {
+  res.sendFile('index.html', options);
 });
-
-server.listen(port, hostname, () => {
+app.get('/about', (req, res) => {
+  res.sendFile('about.html', options);
+});
+app.get('/contact', (req, res) => {
+  res.sendFile('contact-me.html', options);
+});
+app.get('/*splat', (req, res) => {
+  res.sendFile('404.html', options);
+});
+ 
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
